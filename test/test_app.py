8bc4cf1d93dd6bca124b929c52e2fed66194ec7c
Diablo1605 <mkamaleshwaran1605@gmail.com>
2025-11-24 13:22:57 +0530

    added lib

--- test/test_app.py
+++ test/test_app.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from main import app  # Replace 'main' with your app filename if different
+from src.main import app  # Replace 'main' with your app filename if different
 
 client = TestClient(app)
 
